70144904116f440b7290e8520fd903248bcc987d
Dmitry_Plotnikov <dmitry_plotnikov@epam.com>
2020-08-30 14:26:30 +0300

    ported to spring boot

--- src/main/java/com/epam/springcloud/entity/file/FileDownloadDTO.java
+++
@@ -1,9 +0,0 @@
-package com.epam.springcloud.entity.file;
-
-import lombok.Data;
-
-@Data
-public class FileDownloadDTO {
-
-    private Integer id;
-}
--- src/main/java/com/epam/springcloud/resource/SessionAtribute.java
+++
@@ -1,9 +0,0 @@
-package com.epam.springcloud.resource;
-
-public class SessionAtribute {
-
-    public static final String USER_ATTRIBUTE_NAME = "user";
-
-    private SessionAtribute() {
-    }
-}
